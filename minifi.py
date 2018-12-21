b35117a9daf440cd3c810abc3660a09aea8a7593
Timothy Spann <tspann@hortonworks.com>
2018-12-21 17:09:58 -0500

    Update minifi.py

--- minifi.py
+++ minifi.py
@@ -1,3 +1,26 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2
+import argparse
+import os.path
+import re
+import sys
+import tarfile
+import os
+import datetime
+import math
+import random, string
+import base64
+import json
+import time
+from time import sleep
+from time import gmtime, strftime
+
+import numpy as np
+from six.moves import urllib
+import tensorflow as tf
 import sys
 import datetime
 import subprocess
@@ -29,8 +52,161 @@ import ltr559
 import bme680
 from lsm303d import LSM303D
 
-# TODO:  continuous loop, send status message to screen
+tf.logging.set_verbosity(tf.logging.ERROR)
 
+FLAGS = None
+
+ pylint: disable=line-too-long
+DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+# pylint: enable=line-too-long
+
+# yyyy-mm-dd hh:mm:ss
+currenttime= strftime("%Y-%m-%d %H:%M:%S",gmtime())
+
+host = os.uname()[1]
+
+def randomword(length):
+  return ''.join(random.choice(string.lowercase) for i in range(length))
+
+class NodeLookup(object):
+  """Converts integer node ID's to human readable labels."""
+
+  def __init__(self,
+               label_lookup_path=None,
+               uid_lookup_path=None):
+    if not label_lookup_path:
+      label_lookup_path = os.path.join(
+          '/tmp/imagenet', 'imagenet_2012_challenge_label_map_proto.pbtxt')
+    if not uid_lookup_path:
+      uid_lookup_path = os.path.join(
+          '/tmp/imagenet', 'imagenet_synset_to_human_label_map.txt')
+    self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
+
+
+  def load(self, label_lookup_path, uid_lookup_path):
+    """Loads a human readable English name for each softmax node.
+
+    Args:
+      label_lookup_path: string UID to integer node ID.
+      uid_lookup_path: string UID to human-readable string.
+
+    Returns:
+      dict from integer node ID to human-readable string.
+    """
+    if not tf.gfile.Exists(uid_lookup_path):
+      tf.logging.fatal('File does not exist %s', uid_lookup_path)
+    if not tf.gfile.Exists(label_lookup_path):
+      tf.logging.fatal('File does not exist %s', label_lookup_path)
+
+    # Loads mapping from string UID to human-readable string
+    proto_as_ascii_lines = tf.gfile.GFile(uid_lookup_path).readlines()
+    uid_to_human = {}
+    p = re.compile(r'[n\d]*[ \S,]*')
+    for line in proto_as_ascii_lines:
+      parsed_items = p.findall(line)
+      uid = parsed_items[0]
+      human_string = parsed_items[2]
+      uid_to_human[uid] = human_string
+
+    # Loads mapping from string UID to integer node ID.
+    node_id_to_uid = {}
+    proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
+    for line in proto_as_ascii:
+      if line.startswith('  target_class:'):
+        target_class = int(line.split(': ')[1])
+      if line.startswith('  target_class_string:'):
+        target_class_string = line.split(': ')[1]
+        node_id_to_uid[target_class] = target_class_string[1:-2]
+
+    # Loads the final mapping of integer node ID to human-readable string
+    node_id_to_name = {}
+    for key, val in node_id_to_uid.items():
+      if val not in uid_to_human:
+        tf.logging.fatal('Failed to locate: %s', val)
+      name = uid_to_human[val]
+      node_id_to_name[key] = name
+
+    return node_id_to_name
+
+  def id_to_string(self, node_id):
+    if node_id not in self.node_lookup:
+      return ''
+    return self.node_lookup[node_id]
+
+
+def create_graph():
+  """Creates a graph from saved GraphDef file and returns a saver."""
+  # Creates graph from saved graph_def.pb.
+  with tf.gfile.FastGFile(os.path.join(
+      '/tmp/imagenet', 'classify_image_graph_def.pb'), 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+
+def run_inference_on_image(image):
+  """Runs inference on an image.
+
+  Args:
+    image: Image file name.
+
+  Returns:
+    row
+  """
+  if not tf.gfile.Exists(image):
+    tf.logging.fatal('File does not exist %s', image)
+  image_data = tf.gfile.FastGFile(image, 'rb').read()
+
+  # Creates graph from saved GraphDef.
+  create_graph()
+
+  with tf.Session() as sess:
+    # Some useful tensors:
+    # 'softmax:0': A tensor containing the normalized prediction across
+    #   1000 labels.
+    # 'pool_3:0': A tensor containing the next-to-last layer containing 2048
+    #   float description of the image.
+    # 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
+    #   encoding of the image.
+    # Runs the softmax tensor by feeding the image_data as input to the graph.
+    softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
+    predictions = sess.run(softmax_tensor,
+                           {'DecodeJpeg/contents:0': image_data})
+    predictions = np.squeeze(predictions)
+
+    # Creates node ID --> English string lookup.
+    node_lookup = NodeLookup()
+
+	tfrow  = { }
+
+    top_k = predictions.argsort()[-1]
+    human_string = node_lookup.id_to_string(top_k)
+    score = predictions[top_k]
+    tfrow['human_string'] =  str(human_string)
+    tfrow['score'] = str(score)
+    
+    return tfrow
+
+
+def maybe_download_and_extract():
+  """Download and extract model tar file."""
+  dest_directory = '/tmp'
+  if not os.path.exists(dest_directory):
+    os.makedirs(dest_directory)
+  filename = DATA_URL.split('/')[-1]
+  filepath = os.path.join(dest_directory, filename)
+  if not os.path.exists(filepath):
+    def _progress(count, block_size, total_size):
+      sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+          filename, float(count * block_size) / float(total_size) * 100.0))
+      sys.stdout.flush()
+    filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+    print()
+    statinfo = os.stat(filepath)
+    print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+  tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
+# TODO:  send status message to screen
 def do_nothing(obj):
     pass
 
@@ -120,6 +296,9 @@ TCP_PORT = 5005  # define somewhere
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ipaddress, TCP_PORT))
 
+# tensorflow model
+maybe_download_and_extract()
+
 # start camera
 time.sleep(0.5)
 cap = cv2.VideoCapture(0)
@@ -144,7 +323,10 @@ try:
      filename2 = 'images/bog_image_p_{0}.jpg'.format(uuid2)
      cv2.imwrite(filename, frame)
      cpuTemp=int(float(getCPUtemperature()))
+     tfrow = run_inference_on_image(filename)
      end = time.time()
+     row['human_string'] = tfrow['human_string']
+     row['score'] = tfrow['score']
      row['imgname'] = filename
      row['imgnamep'] = filename2
      row['host'] = os.uname()[1]
