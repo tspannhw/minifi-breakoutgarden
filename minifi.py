de80afbe5d2ea46da5dd1a587a7880984b5e680f
Timothy Spann <tspann@hortonworks.com>
2018-12-21 17:10:32 -0500

    Update minifi.py

--- minifi.py
+++ minifi.py
@@ -56,7 +56,7 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 
 FLAGS = None
 
- pylint: disable=line-too-long
+pylint: disable=line-too-long
 DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
 # pylint: enable=line-too-long
 
